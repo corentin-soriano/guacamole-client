984092c47d17a885749d63248b2def94f6a937d8
Corentin SORIANO <csoriano@apache.org>
2025-05-11 15:58:43 +0200

    DEV: webpack dev server

--- guacamole/src/main/frontend/webpack.config.js
+++ guacamole/src/main/frontend/webpack.config.js
@@ -25,18 +25,46 @@ const HtmlPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const webpack = require('webpack');
+const path = require('path');
+
+// cd guacamole/src/main/frontend/
+// npm install
+// npm install -D webpack-cli webpack-dev-server
+// NODE_OPTIONS=--openssl-legacy-provider npx webpack serve
 
 module.exports = {
 
     bail: true,
-    mode: 'production',
+    mode: 'development',
     stats: 'minimal',
 
     output: {
         path: __dirname + '/dist',
-        filename: 'guacamole.[contenthash].js',
+        filename: 'guacamole.[chunkhash].js',
         hashFunction: 'xxhash64',
         clean: true
+    },
+
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, '../../../../guacamole-common-js/src/main/webapp/modules/'),
+            publicPath: '/guacamole-common-js',
+        },
+        proxy: {
+            '/api': {
+                target: 'https://wsl.maincare.dev/guacamole',
+                changeOrigin: true,
+                pathRewrite: { '^/api': '/api' },
+                secure: true
+            },
+            '/websocket-tunnel': {
+                target: 'wss://wsl.maincare.dev/guacamole',
+                ws: true,
+                changeOrigin: true,
+                pathRewrite: { '^/websocket-tunnel': '/websocket-tunnel' },
+                secure: true
+            },
+        },
     },
 
     // Generate source maps
@@ -78,6 +106,7 @@ module.exports = {
         ]
     },
     optimization: {
+        minimize: false,
         minimizer: [
 
             // Minify using Terser
